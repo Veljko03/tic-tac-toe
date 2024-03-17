bdaef7ef6876b6bc4c52966b2dc6105f3f294bd0
Veljko03 <veljkodovragovic@gmail.com>
2024-03-17 22:51:09 +0100

    added players part and changing cell html after each click

--- javaScript.js
+++ javaScript.js
@@ -1,22 +1,61 @@
 
 const GameBoard = (function () {
-    const makeBoard = () =>{
-        const gameBoard = document.querySelector(".gameBoard"); 
-        for(let i=0; i<9;i++){
-            const cell = document.createElement("div");
-            cell.setAttribute("class", "cell")
-            gameBoard.appendChild(cell);
-
-            cell.addEventListener("click", ()=>{
-                
-            })
-        }
-    }
+    let gameboard =["", "","","","","","","",""];
+    const container = document.querySelector(".gameBoard");
     
+    const makeBoard =() => {
+        gameboard.forEach(element => {
+            const cell = document.createElement("div");
+            cell.setAttribute("class", "cell");
+            //cell.innerHTML = playedClick();
+            container.appendChild(cell);
+            cell.addEventListener("click", ()=>{
+                playerControl.playRound();
+                cell.innerHTML = playerControl.getActivePlayer();
+            })
+
+        })
+        
+    }
     return{
         makeBoard,
+        
     }
-    
+})();
+
+
+
+
+const playerControl= (() =>{
+    const players = [
+        {
+            name:"first player",
+            value: "X"
+        },
+        {
+            name:"second player",
+            value: "O"
+        }
+    ];
+    let activePlayerValue = players[1].value;
+    const change = () => {
+        if(activePlayerValue == players[0].value){
+            activePlayerValue=    players[1].value;
+        }else{
+            activePlayerValue = players[0].value;
+        }
+    }
+    const getActivePlayer = () => activePlayerValue;
+    const playRound = () => {
+        
+        change();
+        
+      };
+    return {
+        getActivePlayer,
+        playRound
+
+    }
 })();
 
 
@@ -31,9 +70,19 @@ const GameBoard = (function () {
 
 
 
+
+
+
+
 const btnStart = document.querySelector("#btnStart");
+let boardMade;
 btnStart.addEventListener("click", () => {
-    GameBoard.makeBoard();
+    
+    if(!boardMade){
+        GameBoard.makeBoard();
+        boardMade=true;
+    }
+    
 })
 
 
