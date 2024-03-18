82d188737c64b9769e12d01df32c76a074af0aae
Veljko03 <veljkodovragovic@gmail.com>
2024-03-18 13:14:28 +0100

    fix a bug with displaying players name on click and added winCondition matrix

--- javaScript.js
+++ javaScript.js
@@ -2,19 +2,42 @@
 const GameBoard = (function () {
     let gameboard =["", "","","","","","","",""];
     const container = document.querySelector(".gameBoard");
+    const turn = document.querySelector(".turn");
+    let winCondition= [
+        [ 0, 1, 2 ],
+        [ 3, 4, 5 ], 
+        [ 6, 7, 8 ], 
+        [ 0, 3, 6 ], 
+        [ 1, 4, 7 ], 
+        [ 2, 5, 8 ], 
+        [ 0, 4, 8 ],
+        [ 6, 4, 2 ], 
+    ]
     
     const makeBoard =() => {
-        gameboard.forEach(element => {
+        for(let i=0; i<gameboard.length; i++){
             const cell = document.createElement("div");
+            gameboard[i] = cell.value;
             cell.setAttribute("class", "cell");
             //cell.innerHTML = playedClick();
             container.appendChild(cell);
+            turn.innerHTML = playerControl.getTurn()+"'s turn";
             cell.addEventListener("click", ()=>{
-                playerControl.playRound();
-                cell.innerHTML = playerControl.getActivePlayer();
+                if(cell.innerHTML == ""){
+                    playerControl.playRound();
+                    turn.innerHTML = playerControl.getTurn() +"'s turn";
+                    cell.innerHTML = playerControl.getActivePlayer();
+                    gameboard[i] = playerControl.getActivePlayer();
+                    console.log(gameboard);
+                }
+                
+                
             })
+            
+        }
+            
 
-        })
+        
         
     }
     return{
@@ -38,14 +61,19 @@ const playerControl= (() =>{
         }
     ];
     let activePlayerValue = players[1].value;
+    let activePlayerName = players[0].name;
     const change = () => {
-        if(activePlayerValue == players[0].value){
-            activePlayerValue=    players[1].value;
+        if(activePlayerValue == players[1].value && activePlayerName == players[0].name){
+            activePlayerValue=    players[0].value;
+            activePlayerName = players[1].name;
         }else{
-            activePlayerValue = players[0].value;
+            activePlayerValue = players[1].value;
+            activePlayerName = players[0].name;
         }
     }
     const getActivePlayer = () => activePlayerValue;
+    const getTurn = () => activePlayerName;
+
     const playRound = () => {
         
         change();
@@ -53,7 +81,8 @@ const playerControl= (() =>{
       };
     return {
         getActivePlayer,
-        playRound
+        playRound,
+        getTurn,
 
     }
 })();
